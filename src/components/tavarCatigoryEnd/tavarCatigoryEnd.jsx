7edbbbcc61856b4f3513fbef912ca6a5801ff4e5
abdulazizxoldorov <abdulazizxoldorov05@gmail.com>
2023-09-30 14:14:43 +0500

    frf

--- src/components/tavarCatigoryEnd/tavarCatigoryEnd.jsx
+++ src/components/tavarCatigoryEnd/tavarCatigoryEnd.jsx
@@ -1,108 +1,194 @@
 import React, { useEffect, useState } from 'react'
 import {
-    Table,
-    Thead,
-    Tbody,
-    Tr,
-    Th,
-    Td,
-    TableContainer,
-    Button,
-    Input,
-    Box,
-    Text,
-  } from "@chakra-ui/react";
-  import { Checkbox, CheckboxGroup } from '@chakra-ui/react'
-  import { MdOutlineMoreVert } from "react-icons/md";
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  Button,
+  Input,
+  Box,
+  Text,
+} from "@chakra-ui/react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+
+import { Checkbox, CheckboxGroup } from '@chakra-ui/react'
+import { MdOutlineMoreVert } from "react-icons/md";
 // import TavartableStart from '../tavartableStart/tavartableStart';
 import axios from 'axios';
 import { API } from '../../api';
 import { AiFillMinusCircle, AiFillPlusCircle } from 'react-icons/ai';
-function TavarCatigoryEnd({setCheckVal , handleValCheck}) {
+function TavarCatigoryEnd({ handleValCheck, setCheckVal, setTabsId }) {
+  const [data, setData] = useState([])
+  const [v, setV] = useState('')
+  const [q, setQ] = useState([])
+  let X = []
+  let Y = []
+ 
+  X = data.map((item, i) => item.id)
+  Y = q.map((item, i) => item.id)
 
-  // const [checkedValue,setValue] = useState([])
-  
-  
-  const handleChange = (e) =>{
-    const {value,checked} = e.target
+  const [checkedValues, setValue] = useState([])
+  console.log(checkedValues);
 
-    if(checked){
-      setCheckVal(pre => [...pre,value])
-    }else{
-      setValue(pre =>{
+  const handleChange = (e) => {
+    const { value, checked } = e.target
+
+    if (checked) {
+      setCheckVal(pre => [...pre, value])
+    } else {
+      setValue(pre => {
         return [...pre.filter(skill => skill === value)]
       })
     }
   }
 
-    // const monthNames = [
-    //   "January",
-    //   "February",
-    //   "March",
-    //   "April",
-    //   "May",
-    //   "June",
-    //   "July",
-    //   "August",
-    //   "September",
-    //   "October",
-    //   "November",
-    //   "December",
-    // ];
-    // const d = new Date();
-    // let name = monthNames[d.getMonth()];
-    // const [open, setopen] = useState(false);
-    // const handleClick = () => setopen(!open);
-    const [data,setData] = useState([])
-    useEffect(() => {
-        axios
-          .get(`${API}api/category-types`, {
-            headers: {
-              // "ngrok-skip-browser-warning": true,
-              // "Access-Control-Allow-Origin": "*",
-              Authorization: `Bearer ${localStorage.getItem("token")}`,
-            },
-          })
-          .then((res) => {
-            setData(res.data);
-          });
-      }, []);
+
+  const handeId = (e) => {
+    const { value, click } = e.target
+    if (click) {
+      setTabsId(pre => [...pre, value])
+    } else {
+      setTabsId(pre => {
+        return [...pre.filter(skills => skills === value)]
+      })
+    }
+  }
+
+
+  useEffect(() => {
+    axios
+      .get(`${API}api/category-types`, {
+        headers: {
+          // "ngrok-skip-browser-warning": true,
+          // "Access-Control-Allow-Origin": "*",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((res) => {
+        setData(res.data);
+      });
+  }, []);
+
+
+  const [data1, setData1] = useState([])
+  useEffect(() => {
+    axios
+      .get(`${API}api/category`, {
+        headers: {
+          // "ngrok-skip-browser-warning": true,
+          // "Access-Control-Allow-Origin": "*",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((res) => {
+        setData1(res.data);
+      });
+  }, []);
+
 
   return (
     <Box>
-        <TableContainer shadow={"0px 2px 8px 0px rgba(0, 0, 0, 0.12)"}>
-          <Table width={"100%"} rounded={"16px"} fontSize={'19px'}>
-            <Thead>
-              <Tr bg="#F1F3F9" >
-                <Th fontWeight={'bold'} color={'#1D2433'} textTransform={'capitalize'} fontSize={'17px'}>№</Th>
-                <Th fontWeight={'bold'} color={'#1D2433'} textTransform={'capitalize'} fontSize={'17px'}> Nomi</Th>
-                <Th fontWeight={'bold'} color={'#1D2433'} textTransform={'capitalize'} fontSize={'17px'}>Qo’shilgan sana</Th>
-                <Th fontWeight={'bold'} color={'#1D2433'} textTransform={'capitalize'} fontSize={'17px'}>User</Th>
-                <Th></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {data.map((item,i) =>(
-                <Tr key={i} bg={i % 2 == 1 ? '#F8F9FC' : ''}>
-                  
-                  <Td w={'0%'}  ><Checkbox size='lg' colorScheme='purple' value={item.id} checked={item.id == item.productCategoryTypes ? true : false} onChange={handleChange} ></Checkbox></Td>
-                  <Td  w={'50%'}>{item.name}</Td>
-                  <Td> {String(item.date).slice(0, 4) +
-                    " " +
-                    `${name}` +
-                    " " +
-                    String(item.date).slice(8, 10) +
-                    " " +
-                    String(item.date).slice(11, 16)}</Td>
-                  <Td>Komiljon Soliyev Xaydarovich</Td>
-                </Tr>    
-              ))}
-                          
-            </Tbody>
-          </Table>
-        </TableContainer>
-        <Button onClick={handleValCheck}>Send</Button>
+
+      <Box >
+        <Tabs colorScheme='blue'>
+          <TabList>
+            {data1.map((item, i) => (
+              <Box key={i} >
+                <Tab onClick={function () {
+                  setV(item.name)
+                  setQ(item.productCategoryTypes)
+
+                }
+
+                } value={item.id} onChange={setTabsId(item.id)} >{item.name}</Tab>
+              </Box>
+            ))}
+
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+
+            </TabPanel>
+            <TabPanel>
+
+            </TabPanel>
+            <TabPanel>
+
+              <Text></Text>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
       </Box>
+
+
+      <TableContainer shadow={"0px 2px 8px 0px rgba(0, 0, 0, 0.12)"}>
+        <Table width={"100%"} rounded={"16px"} fontSize={'19px'}>
+          <Thead>
+            <Tr bg="#F1F3F9" >
+              <Th fontWeight={'bold'} color={'#1D2433'} textTransform={'capitalize'} fontSize={'17px'}>№</Th>
+              <Th fontWeight={'bold'} color={'#1D2433'} textTransform={'capitalize'} fontSize={'17px'}> Nomi</Th>
+              <Th fontWeight={'bold'} color={'#1D2433'} textTransform={'capitalize'} fontSize={'17px'}>Qo’shilgan sana</Th>
+              <Th fontWeight={'bold'} color={'#1D2433'} textTransform={'capitalize'} fontSize={'17px'}>User</Th>
+              <Th></Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+
+            {data.map(function (item, i) {
+              if (Y.includes(item.id)) {
+                return (
+                  <Tr key={i} bg={i % 2 == 1 ? '#F8F9FC' : ''}>
+
+                    <Td w={'0%'}>  <Checkbox size='lg' colorScheme='purple' value={item.id} isChecked onChange={handleChange} ></Checkbox></Td>
+                    <Td w={'50%'}>{item.name}</Td>
+                    <Td> {String(item.date).slice(0, 4) +
+                      " " +
+                      `${name}` +
+                      " " +
+                      String(item.date).slice(8, 10) +
+                      " " +
+                      String(item.date).slice(11, 16)}</Td>
+                    <Td>Komiljon Soliyev Xaydarovich</Td>
+                  </Tr>
+                )
+              } else {
+                return (
+                  <Tr key={i} bg={i % 2 == 1 ? '#F8F9FC' : ''}>
+
+                    <Td w={'0%'}>  <Checkbox size='lg' colorScheme='purple' value={item.id} onChange={handleChange} ></Checkbox></Td>
+                    <Td w={'50%'}>{item.name}</Td>
+                    <Td> {String(item.date).slice(0, 4) +
+                      " " +
+                      `${name}` +
+                      " " +
+                      String(item.date).slice(8, 10) +
+                      " " +
+                      String(item.date).slice(11, 16)}</Td>
+                    <Td>Komiljon Soliyev Xaydarovich</Td>
+                  </Tr>
+                )
+              }
+            })}
+
+          </Tbody>
+        </Table>
+      </TableContainer>
+      <Box display={'flex'} alignItems={'flex-end'} w={'100%'} justifyContent={'flex-end'}>
+      <Button _hover={'none'} _active={'none'} mt={'15px'} bg={'green'} color={'white'} onClick={handleValCheck}>Send</Button>
+      </Box>
+
+      {X.map(function (l) {
+        if (Y.includes(l)) {
+          console.log(true);
+        } else {
+          console.log(false);
+        }
+      })}
+
+    </Box>
   )
 }
 
